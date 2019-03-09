c81cd439da1cb9051720f0b6a9c5628e7486ecd7
WILLIAMNGUYEN18 <nguyew2@uw.edu>
2019-03-08 23:15:52 -0800

    Added BUS, PARK_AND_RIDE, and TRANSIT_CARD inserts

---
+++ PARK_AND_RIDE_INSERT.sql
@@ -0,0 +1,13 @@
+--PARK_AND_RIDE
+
+--fail
+INSERT INTO PARK_AND_RIDE VALUES('parkaddress1','parkname1', 0,0);
+INSERT INTO PARK_AND_RIDE VALUES('parkaddress2','parkname2', 10,-1);
+INSERT INTO PARK_AND_RIDE VALUES('parkaddress3','parkname3', 1000001,0);
+
+--min
+INSERT INTO PARK_AND_RIDE VALUES('parkaddress4','parkname4', 1,0);
+
+--max
+INSERT INTO PARK_AND_RIDE VALUES('parkaddress5','parkname5', 100000,13245);
+
