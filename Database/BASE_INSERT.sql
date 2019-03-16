b587f177623fdae413ead1ef8dc3ce4307aa0ddc
austi <austin.green@outlook.com>
2019-03-15 18:28:56 -0700

    Add real base data

--- Database/BASE_INSERT.sql
+++ Database/BASE_INSERT.sql
@@ -1,8 +1,8 @@
-INSERT INTO BASE Values ("985 Legal NE CT", "Seattle County", 9);
-INSERT INTO BASE Values ("985 Legal NE CT", "Seattle County", 10);
-INSERT INTO BASE Values ("985 Legal NE CT", "Seattle County", 9);
-INSERT INTO BASE Values ("985 Legal NE CT", "Seattle County", 8);
-INSERT INTO BASE Values ("asdfafasdf", "Renton County", 7);
-INSERT INTO BASE Values ("985 Legal NE CT", "Renton County", 7);
-INSERT INTO BASE Values ("985 Legal NE CT", "Renton County", 7);
-INSERT INTO BASE Values ("985 Legal NE CT", "Renton County", 7);
+INSERT INTO BASE VALUES ("1220 4th Ave S, Seattle, WA 98134", "Ryerson Base", 150);
+INSERT INTO BASE VALUES ("640 S Massachusetts St, Seattle, WA 98134", "Atlantic Base", 220);
+INSERT INTO BASE VALUES ("1500 6th Ave S, Seattle, WA 98134", "Central Base", 120);
+INSERT INTO BASE VALUES ("1790 124th Ave NE, Bellevue, WA 98005", "Bellevue Base", 150);
+INSERT INTO BASE VALUES ("1975 124th Ave NE, Bellevue, WA 98005", "East Base", 170);
+INSERT INTO BASE VALUES ("12100 East Marginal Way South, Tukwila, WA 98168", "South Base", 200);
+INSERT INTO BASE VALUES ("2160 N 163rd St, Shoreline, WA 98133", "North Base", 190);
+
