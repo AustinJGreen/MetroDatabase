eda79a4d520f0b2f07c9892e9622aad6dafef269
austi <austin.green@outlook.com>
2019-03-15 18:03:38 -0700

    Add webserver

--- BASE_INSERT.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO BASE Values ("985 Legal NE CT", "Seattle County", 9);
-INSERT INTO BASE Values ("985 Legal NE CT", "Seattle County", 10);
-INSERT INTO BASE Values ("985 Legal NE CT", "Seattle County", 9);
-INSERT INTO BASE Values ("985 Legal NE CT", "Seattle County", 8);
-INSERT INTO BASE Values ("asdfafasdf", "Renton County", 7);
-INSERT INTO BASE Values ("985 Legal NE CT", "Renton County", 7);
-INSERT INTO BASE Values ("985 Legal NE CT", "Renton County", 7);
-INSERT INTO BASE Values ("985 Legal NE CT", "Renton County", 7);
