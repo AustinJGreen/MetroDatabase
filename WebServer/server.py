e44740277f276f684b09188ae9bbf08b4d32755a
ajosg <austin.green@outlook.com>
2019-03-18 18:46:40 +0000

    Add query and add more columns to queries

--- WebServer/server.py
+++ WebServer/server.py
@@ -3,6 +3,9 @@ from metrodb import MetroDB
 import os
 import ssl
 
+class Custom(object):
+    pass
+
 context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
 context.load_cert_chain('cert.pem', 'key.pem')
 
@@ -28,6 +31,18 @@ def get_stop_names():
         stopList.append(stop[0])
     return stopList
 
+def get_pandr():
+    result = db.execute_command("SELECT Park_City, Park_Address, Park_Name, Park_Spots FROM PARK_AND_RIDE ORDER BY Park_Spots DESC")
+    pandrList = []
+    for pandr in result:
+        o = Custom()
+        o.city = pandr[0]
+        o.addr = pandr[1]
+        o.name = pandr[2]
+        o.spots = pandr[3]
+        pandrList.append(o)
+    return pandrList
+
 @app.route('/')
 def homepage():
     return render_template('index.html')
@@ -40,11 +55,14 @@ def query1():
 @app.route('/query1', methods=['POST'])
 def handle_query1():
     routeNum = request.form['Route_number']
-    stops = db.execute_command("SELECT BS.Stop_Name FROM BUS_ROUTE AS BR, BUS_STOP AS BS, BUS_ROUTE_STOPS AS BRS WHERE BRS.Stop_ID = BS.Stop_ID AND BRS.Route_number = BR.Route_number AND BR.Route_number = \'" + str(routeNum) +"\' GROUP BY BS.Stop_Name;")
+    stops = db.execute_command("SELECT BS.Stop_Name, BS.Cross_Street FROM BUS_ROUTE AS BR, BUS_STOP AS BS, BUS_ROUTE_STOPS AS BRS WHERE BRS.Stop_ID = BS.Stop_ID AND BRS.Route_number = BR.Route_number AND BR.Route_number = \'" + str(routeNum) +"\' GROUP BY BS.Stop_Name, BS.Cross_Street;")
 
     arr = []
     for stop in stops:
-        arr.append(stop[0])
+        o = Custom()
+        o.name = stop[0]
+        o.addr = stop[1]
+        arr.append(o)
 
     routeList=get_routes()
     return render_template('query1.html', routeList=routeList,ds=True,stops=arr)
@@ -58,14 +76,23 @@ def query2():
 def handle_query2():
     stopA = request.form['stop_a']
     stopB = request.form['stop_b']
-    routes = db.execute_command("SELECT BR.Route_number FROM BUS_ROUTE BR WHERE \'" + str(stopA) + "\' IN (SELECT Stop_Name FROM BUS_STOP A JOIN BUS_ROUTE_STOPS B ON A.Stop_ID=B.Stop_ID WHERE B.Route_number=BR.Route_number) AND \'" + str(stopB) + "\' IN (SELECT Stop_Name FROM BUS_STOP A JOIN BUS_ROUTE_STOPS B ON A.Stop_ID=B.Stop_ID WHERE B.Route_number=BR.Route_number) GROUP BY BR.Route_number")
+    routes = db.execute_command("SELECT BR.From_name, BR.To_name, BR.Route_number FROM BUS_ROUTE BR WHERE \'" + str(stopA) + "\' IN (SELECT Stop_Name FROM BUS_STOP A JOIN BUS_ROUTE_STOPS B ON A.Stop_ID=B.Stop_ID WHERE B.Route_number=BR.Route_number) AND \'" + str(stopB) + "\' IN (SELECT Stop_Name FROM BUS_STOP A JOIN BUS_ROUTE_STOPS B ON A.Stop_ID=B.Stop_ID WHERE B.Route_number=BR.Route_number) GROUP BY BR.From_name, BR.To_name, BR.Route_number")
     
     arr = []
     for route in routes:
-        arr.append(route[0])
+        o = Custom()
+        o.f = route[0]
+        o.t = route[1]
+        o.n = route[2]
+        arr.append(o)
 
     stopList = get_stop_names()
     return render_template('query2.html',stopList=stopList,ds=True,routes=arr)
+
+@app.route('/query3', methods=['GET'])
+def query3():
+    pandrs = get_pandr()
+    return render_template('query3.html',pandrs=pandrs)
 
 if __name__ == '__main__':
     sess.init_app(app)
