27a9e611bfc5870f39c4042bc6a7f184946700d6
ajosg <austin.green@outlook.com>
2019-03-18 04:43:38 +0000

    Reorganize tests

--- WebServer/dbconnect.py
+++
@@ -1,9 +0,0 @@
-import MySQLdb
-def connection():
-    conn = MySQLdb.connect(host='metrodb.c6yfhb0actbf.us-west-2.rds.amazonaws.com',
-                           user = 'guest',
-                           passwd = 'guest',
-                           db = 'metrodb')
-    c = conn.cursor()
-
-    return c, conn
